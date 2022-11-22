d69b0317de6ec55008bf30fd131bc109cc658c48
Maety <maetyuryu@gmail.com>
2022-11-22 22:14:39 +0900

    [add]明日書くものを先に作った

---
+++ Assets/Scripts/InputText.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputText : MonoBehaviour
+{
+    //Œˆ‚ß‚ç‚ê‚½•¶š‚Ì‚Ç‚ê‚©‚ğ“ü—Í‚³‚¹‚±‚±‚Å”»’è‚·‚é
+    //ˆá‚¤ê‡‚Í‚à‚¤ˆê“x‘I‘ğ•¶‚ğo—Í‚µ‚½‚¢
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
