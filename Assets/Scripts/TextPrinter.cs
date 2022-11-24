46af2ffa51f8060d7911fc36fbd98a8000390fff
Maety <maetyuryu@gmail.com>
2022-11-24 18:04:54 +0900

    [update]文字を出力した後文章が見やすいように自動でスクロールするようにした

--- Assets/Scripts/TextPrinter.cs
+++ Assets/Scripts/TextPrinter.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Collections;
 using TMPro;
 using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextPrinter : MonoBehaviour
 {
@@ -10,29 +13,40 @@ public class TextPrinter : MonoBehaviour
     [SerializeField]
     private string[] _messages;
 
+    [SerializeField]
+    ScrollRect _sr;
+
     public string[] MessageSet
     {
         set
         {
             _messages = value;
+            _textUi.text += '\n';
+            Print();
         }
     }
 
-    private void Start()
-    {
-        _textUi.text = "";
-        Print();
-    }
-
-    public void Print() 
+    public void Print()
     {
         if (_messages != null)
         {
-            foreach (var message in _messages) 
+            foreach (var message in _messages)
             {
                 _textUi.text += message;
                 _textUi.text += '\n';
             }
+            StartCoroutine(ForceScrollDown());
         }
+    }
+
+    IEnumerator ForceScrollDown()
+    {
+
+        // 1フレーム待たないと完全に実行されない
+
+        yield return new WaitForEndOfFrame();
+
+        _sr.verticalNormalizedPosition = 0.0f;
+
     }
 }
