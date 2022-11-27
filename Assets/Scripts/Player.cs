02196e3f236def0a88755f9b07989aa6b60a862d
Maety <maetyuryu@gmail.com>
2022-11-28 00:59:25 +0900

    [update]5日目最終コミット！

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions;
+using Unity.VisualScripting;
 
 public class Player : MonoBehaviour
 {
@@ -10,11 +12,13 @@ public class Player : MonoBehaviour
     [SerializeField]
     private Room _playerRoom;
 
-    public Room PlayerRoom 
+    private Regex roomRegex = new Regex("•”‰®");
+
+    public Room PlayerRoom
     {
         get { return _playerRoom; }
 
-        set 
+        set
         {
             //î•ñ‚ª‚È‚¢‚Æ‚«‚Í•”‰®î•ñ‚ğo—Í‚µ‚ÄI‚í‚é
             if (value == null) { RoomMessageSet(); return; }
@@ -29,8 +33,37 @@ public class Player : MonoBehaviour
         RoomMessageSet();
     }
 
-    private void RoomMessageSet() 
+    private void RoomMessageSet()
     {
-        _tp.MessageTextSet = _playerRoom.texts;
+        if (_playerRoom.ItemNotSearchtexts.Length == 0)
+        {
+            _tp.MessageTextSet = _playerRoom.texts;
+        }
+        else
+        {
+            _tp.MessageTextSet = _playerRoom.ItemNotSearchtexts;
+        }
+
+        if (roomRegex.IsMatch(_playerRoom.name))
+        { RoomCenter(); }
+    }
+
+    public void RoomCenter()
+    {
+        string[] rooms = new string[4];
+
+        if (_playerRoom.rooms[0] != null)
+        { rooms[0] = $"“Œ :{_playerRoom.rooms[0].name}"; }
+
+        if (_playerRoom.rooms[1] != null)
+        { rooms[1] = $"¼ :{_playerRoom.rooms[1].name}"; }
+
+        if (_playerRoom.rooms[2] != null)
+        { rooms[2] = $"“ì :{_playerRoom.rooms[2].name}"; }
+
+        if (_playerRoom.rooms[3] != null)
+        { rooms[3] = $"–k :{_playerRoom.rooms[3].name}"; }
+
+        _tp.CenterRoomText(rooms);
     }
 }
