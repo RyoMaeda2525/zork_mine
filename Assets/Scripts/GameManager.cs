7da96bc9c0519eae8d0bdd21c7a37d9152053ce4
Maety <maetyuryu@gmail.com>
2022-11-28 00:55:54 +0900

    [update]プレイヤーに必要なコマンドなどは全て追加した

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,93 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool _gameClear = false;
+
+    [SerializeField]
+    private RoomManager _roomManager;
+
+    [SerializeField]
+    private TextPrinter _tp;
+
+    [SerializeField, Tooltip("ゲームクリアの判定な部屋情報")]
+    private Room _judgeRoom;
+
+    [SerializeField, Tooltip("ゲームクリアの判定なアイテム情報")]
+    private string[] _judgeItemName;
+
+    private List<RoomItem> _deadItems;
+
+    public void Ending()
     {
-        
+        _gameClear = true;
+
+        foreach (var item in _deadItems)
+        {
+
+            if (item.itemName == "少女")
+            {
+                if (_deadItems.Count > 7)
+                {
+                    _tp.TextPrint("水槽に入れると、死んだはずの少女が目の前に現れ");
+                    _tp.TextPrint("あなたに「ありがとう」と言って天に旅立ちます。");
+                }
+                else
+                {
+                    _tp.TextPrint("水槽に入れると、どこからか大きな嘆きや怨嗟の声が聞こえてきます。");
+                    _tp.TextPrint("しかし数秒すると静かになり、死んだはずの少女が目の前に現れ");
+                    _tp.TextPrint("あなたに「ありがとう」と言って天に旅立ちます。");
+                }
+                _tp.TextPrint(" おめでとうございます！ゲームクリアです！");
+                break;
+            }
+            else //badEnd確定
+            {
+                if (Judge(_judgeItemName[0]) && Judge(_judgeItemName[1]))
+                {
+                    _tp.TextPrint("水槽に入れると、少女は嬉しそうにする");
+                    _tp.TextPrint("しかし、そのほほ笑みは次第に狂気に染まり");
+                    _tp.TextPrint("狂ったように笑い始めます。");
+                    _tp.TextPrint("「これでようやく解放された。」");
+                    _tp.TextPrint("と口を動かした次の瞬間、部屋中に怪物が出現し");
+                    _tp.TextPrint("あなたはなぶり殺されました。");
+                }
+                else
+                {
+                    _tp.TextPrint("水槽に入れると、少女は嬉しそうにするが");
+                    _tp.TextPrint("段々と顔が歪んでいき、ガラス越しで");
+                    _tp.TextPrint("「なんであいつらは幸福なのよ」");
+                    _tp.TextPrint("と口を動かした次の瞬間、部屋中に怪物が出現し");
+                    _tp.TextPrint("あなたはなぶり殺されました。");
+                }
+                _tp.TextPrint("BadEndです。");
+                break;
+            }
+        }
+        _tp.TextPrint("プレイありがとうございました。");
+        _tp.TextPrint("「終了」と入力することでゲームを終了できます。");
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DeadItemSet(RoomItem deadItem) { _deadItems.Add(deadItem); }
+
+    private void OnApplicationQuit()
     {
-        
+        _roomManager.RoomReset();
+    }
+
+    private bool Judge(string itemName)
+    {
+        if (_judgeRoom != null)
+        {
+            for (int i = 0; i < _judgeRoom.roomItemList.Count; i++)
+            {
+                if (_judgeRoom.roomItemList[i].itemName == itemName)
+                { return true; }
+            }
+            return false;
+        }
+        return false;
     }
 }
--- Assets/Scripts/InputText.cs
+++ Assets/Scripts/InputText.cs
@@ -1,9 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
-using UnityEngine.LowLevel;
 using System.Text.RegularExpressions;
 
 public class InputText : MonoBehaviour
@@ -15,6 +11,12 @@ public class InputText : MonoBehaviour
     private Player _player;
 
     [SerializeField]
+    private Inventory _inventory;
+
+    [SerializeField]
+    GameManager _gm;
+
+    [SerializeField]
     private TextPrinter _tp;
 
     private Regex _get = new Regex("を取る");
@@ -23,13 +25,13 @@ public class InputText : MonoBehaviour
 
     private Regex _open = new Regex("を開ける");
 
-    private Regex _use = new Regex("を使う");
+    private Regex _use = new Regex("使う");
 
     //決められた文字のどれかを入力させここで判定する
     //違う場合はもう一度選択文を出力したい
     public void TextInput()
     {
-        if (_inputField.text != "")
+        if (_inputField.text != "" && !_gm._gameClear)
         {
             _tp.InputTextPrint(_inputField.text);
 
@@ -64,96 +66,256 @@ public class InputText : MonoBehaviour
                     break;
 
                 case "位置":
+                    _tp.TextPrint(_player.PlayerRoom.name);
+                    _player.RoomCenter();
+                    break;
 
+                case "アイテム":
+                    if (_inventory._roomItems.Count > 0)
+                    {
+                        _tp.TextPrint("アイテム一覧");
+                        string[] ItemNames = new string[_inventory._roomItems.Count];
+                        for (int i = 0; i < _inventory._roomItems.Count; i++)
+                        {
+                            ItemNames[i] = _inventory._roomItems[i].itemName;
+                        }
+                        _tp.MessageTextSet = ItemNames;
+                    }
+                    else { _tp.TextPrint("所持しているアイテムはない"); }
+                    break;
 
+                case "終了":
+                    Application.Quit();
+                    break;
                 default:
                     //ものを取ると書いた時の処理
                     if (_get.IsMatch(_inputField.text))
                     {
-                        for (int i = 0; i < _player.PlayerRoom.roomItemList.Count; i++)
-                        {
-                            Regex regex = new Regex(_player.PlayerRoom.roomItemList[i].itemName);
-
-                            if (regex.IsMatch(_inputField.text))
-                            {
-                                for (int j = 0; j < _tp.MessageTextSet.Length; j++)
-                                {
-                                    if (regex.IsMatch(_tp.MessageTextSet[j]))
-                                    {
-
-                                        _tp.MessageTextSet = _player.PlayerRoom.roomItemList[i].getText;
-                                        _inputField.text = "";
-                                        return;
-                                    }
-                                }
-                            }
-                        }
-                        Debug.Log("取るものがありませんでした");
+                        GetItem();
                     }
                     //ものを見ると書いた時の処理
                     else if (_look.IsMatch(_inputField.text))
                     {
-                        for (int i = 0; i < _player.PlayerRoom.roomItemList.Count; i++)
-                        {
-                            Regex regex = new Regex(_player.PlayerRoom.roomItemList[i].itemName);
-
-                            if (regex.IsMatch(_inputField.text))
-                            {
-                                for (int j = 0; j < _tp.MessageTextSet.Length; j++)
-                                {
-                                    if (regex.IsMatch(_tp.MessageTextSet[j]))
-                                    {
-                                        _tp.MessageTextSet = _player.PlayerRoom.roomItemList[i].informationText;
-                                        _inputField.text = "";
-                                        return;
-                                    }
-                                }
-                            }
-                        }
-
-                        Debug.Log("見るものがありませんでした");
+                        LookItem();
                     }
                     //ものを開けると書いた時の処理
                     else if (_open.IsMatch(_inputField.text))
                     {
-                        for (int i = 0; i < _player.PlayerRoom.roomItemList.Count; i++)
-                        {
-                            Regex regex = new Regex(_player.PlayerRoom.roomItemList[i].itemName);
-
-                            if (regex.IsMatch(_inputField.text))
-                            {
-                                for (int j = 0; j < _tp.MessageTextSet.Length; j++)
-                                {
-                                    if (regex.IsMatch(_tp.MessageTextSet[0]) || regex.IsMatch(_player.PlayerRoom.texts[j]))
-                                    {
-                                        Regex regexDoor = new Regex("扉");
-
-                                        if (regexDoor.IsMatch(_player.PlayerRoom.roomItemList[i].itemName))
-                                        {
-                                            RoomSet(_player.PlayerRoom.roomItemList[i].room);
-                                        }
-                                        else 
-                                        {
-                                            _tp.MessageTextSet = _player.PlayerRoom.roomItemList[i].openText;
-                                        }
-                                        _inputField.text = "";
-                                        return;
-                                    }
-                                }
-                            }
-                        }
-
-                        Debug.Log("開けるものがありませんでした");
+                        OpenItem();
                     }
                     //ものを使うと書いた時の処理
                     else if (_use.IsMatch(_inputField.text))
                     {
-
+                        UseItem();
                     }
                     break;
             }
-            _inputField.text = "";
         }
+        else if (_gm._gameClear) 
+        {
+            _tp.InputTextPrint(_inputField.text);
+            if(_inputField.text == "終了") 
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+    private void GetItem()
+    {
+        for (int i = 0; i < _player.PlayerRoom.roomItemList.Count; i++)
+        {
+            Regex regex = new Regex(_player.PlayerRoom.roomItemList[i].itemName);
+
+            if (regex.IsMatch(_inputField.text))
+            {
+                if (TextIsMatch(_tp.MessageTextSet, regex) || TextIsMatch(_player.PlayerRoom.texts, regex))
+                {
+                    if (_inventory.ItemSearch(_player.PlayerRoom.roomItemList[i]))
+                    {
+                        RoomItem seachItem = _player.PlayerRoom.roomItemList[i];
+
+                        if (seachItem.lifeBool && seachItem.deadBool)
+                        {
+                            _tp.TextPrint("意識がない少女を本ごと抱えた。");
+                        }
+                        else { _tp.MessageTextSet = seachItem.getText; }
+
+                        _inputField.text = "";
+
+                        if (seachItem.collectItemBool)
+                        {
+                            _inventory._roomItems.Add(seachItem);
+                            if (seachItem.getItemName.Length > 0)
+                            {
+                                for (int j = 0; j < _player.PlayerRoom.roomItemList.Count; j++)
+                                {
+                                    foreach (var itemName in seachItem.getItemName)
+                                    {
+                                        if (_player.PlayerRoom.roomItemList[j].itemName == itemName)
+                                        {
+                                            _inventory._roomItems.Add(_player.PlayerRoom.roomItemList[j]);
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                            _player.PlayerRoom.ItemNotSearchtexts = seachItem.notFaundText;
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+        _inputField.text = "";
+        Debug.Log("取るものがありませんでした");
+    }
+
+    private void LookItem()
+    {
+        for (int i = 0; i < _player.PlayerRoom.roomItemList.Count; i++)
+        {
+            RoomItem item = _player.PlayerRoom.roomItemList[i];
+
+            Regex regex = new Regex(item.itemName);
+
+            if (regex.IsMatch(_inputField.text))
+            {
+                if (TextIsMatch(_tp.MessageTextSet, regex) || TextIsMatch(_player.PlayerRoom.texts, regex))
+                {
+                    if (item.lifeBool && item.deadBool)
+                    {
+                        _tp.MessageTextSet = _player.PlayerRoom.roomItemList[i].DiedText;
+                    }
+                    else { _tp.MessageTextSet = _player.PlayerRoom.roomItemList[i].informationText; }
+                    _inputField.text = "";
+                    return;
+                }
+            }
+        }
+
+        for (int i = 0; i < _inventory._roomItems.Count; i++)
+        {
+            RoomItem item = _inventory._roomItems[i];
+
+            Regex regex = new Regex(item.itemName);
+
+            if (item.lifeBool && item.deadBool)
+            {
+                _tp.MessageTextSet = _player.PlayerRoom.roomItemList[i].DiedText;
+            }
+            else { _tp.MessageTextSet = _player.PlayerRoom.roomItemList[i].informationText; }
+            _inputField.text = "";
+            return;
+
+        }
+        _inputField.text = "";
+        Debug.Log("見るものがありませんでした");
+    }
+
+    private void OpenItem()
+    {
+        for (int i = 0; i < _player.PlayerRoom.roomItemList.Count; i++)
+        {
+            Regex regex = new Regex(_player.PlayerRoom.roomItemList[i].itemName);
+
+            if (regex.IsMatch(_inputField.text))
+            {
+                if (TextIsMatch(_tp.MessageTextSet, regex) || TextIsMatch(_player.PlayerRoom.texts, regex))
+                {
+                    Regex regexDoor = new Regex("扉");
+
+                    if (regexDoor.IsMatch(_player.PlayerRoom.roomItemList[i].itemName))
+                    {
+                        _tp.TextPrint("扉を開き奥の部屋に入った。");
+                        RoomSet(_player.PlayerRoom.roomItemList[i].room);
+                    }
+                    else
+                    {
+                        _tp.MessageTextSet = _player.PlayerRoom.roomItemList[i].openText;
+                    }
+                    _inputField.text = "";
+                    return;
+                }
+            }
+        }
+
+        for (int i = 0; i < _inventory._roomItems.Count; i++)
+        {
+            Regex regex = new Regex(_inventory._roomItems[i].itemName);
+
+            if (regex.IsMatch(_inputField.text))
+            {
+                _tp.MessageTextSet = _player.PlayerRoom.roomItemList[i].openText;
+                _inputField.text = "";
+                return;
+            }
+        }
+
+        _inputField.text = "";
+        Debug.Log("開けるものがありませんでした");
+    }
+
+    private void UseItem()
+    {
+        for (int i = 0; i < _inventory._roomItems.Count; i++)
+        {
+            Regex regex = new Regex("^" + _inventory._roomItems[i].itemName);
+
+            if (regex.IsMatch(_inputField.text))
+            {
+                RoomItem useToItem = UseToItemSeach(_inventory._roomItems[i]);
+
+                if (useToItem != null)
+                {
+                    _inputField.text = "";
+                    _inventory.Use(_inventory._roomItems[i], useToItem);
+                    break;
+                }
+            }
+
+        }
+        _inputField.text = "";
+        Debug.Log("使用できません");
+    }
+
+    private bool TextIsMatch(string[] texts, Regex regex)
+    {
+        foreach (var text in texts)
+        {
+            if (regex.IsMatch(text))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private RoomItem UseToItemSeach(RoomItem item)
+    {
+        string itemSearch = _inputField.text.Replace(item.itemName, "");
+
+        for (int i = 0; i < _inventory._roomItems.Count; i++)
+        {
+            Regex regex = new Regex(_inventory._roomItems[i].itemName);
+
+            if (regex.IsMatch(itemSearch))
+            {
+                return _inventory._roomItems[i];
+            }
+        }
+
+        for (int i = 0; i < _player.PlayerRoom.roomItemList.Count; i++)
+        {
+            Regex regex = new Regex(_player.PlayerRoom.roomItemList[i].itemName);
+
+            if (regex.IsMatch(itemSearch))
+            {
+                return _player.PlayerRoom.roomItemList[i];
+            }
+        }
+
+        return null;
     }
 
     public void RoomSet(Room room)
